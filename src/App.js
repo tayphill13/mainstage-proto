6d99974d1bb9f00489a6b29805ddc691f54ac213
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-06 10:13:34 -0700

    begin new tutorial project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Navbar from '../components/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Main Stage</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+
+function NavBar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className="navbar-logo">
+            MNSTG <i className='fab fa-typo3' />
+          </Link>
+        </div>
+      </nav>
+
+    </>
+  )
+}
+
+export default NavBar
