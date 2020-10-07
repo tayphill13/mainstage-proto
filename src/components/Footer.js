2976554cc028728283c382a1ca0899adff5b5674
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-06 17:09:11 -0700

    add the social media logos to the bottom of the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ function Footer() {
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
-            <Link to='/contact'>Twitter</Link>
+            <Link to='/'>Twitter</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
@@ -51,6 +51,41 @@ function Footer() {
           <div className="footer-logo">
             <Link to='/' className='social-logo'>
               MainStage <i className='fas fa-couch' />
+            </Link>
+          </div>
+          <small className='website-rights'>MainStage © 2020</small>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i className='fab fa-facebook-f' />
+            </Link>
+            <Link
+              className='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i className='fab fa-instagram' />
+            </Link>
+            <Link
+              className='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i className='fab fa-twitter' />
+            </Link>
+            <Link
+              className='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='YouTube'
+            >
+              <i className='fab fa-youtube' />
             </Link>
           </div>
         </div>
