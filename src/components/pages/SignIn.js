d0b342468189213f2b63f92356ed167fb174e5dd
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-07 12:34:00 -0700

    continuig to work on firebase auth

--- src/components/pages/SignIn.js
+++ src/components/pages/SignIn.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../../App.css';
+import app from "./firebase";
 
 export default function SignIn() {
-  return <h1 className='sign-in'>Sign in for returning artists</h1>;
+  return <h1 className='sign-in'>Sign in for returning artists</h1>,
+  <button onClick={() => app.auth().signOut()}>Sign out</button>
 }
