4db6db0ab776f6c70fedd1c4caf90243eb8924e8
Taylor Phillips <taylorphillips133@gmail.com>
2020-10-08 09:46:19 -0700

    add the about information to the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function About() {
-  return (<h1 className='about'>ABOUT</h1>);
+  return <h1 className='about'>There are plenty of platforms that offer music like Spotify, iTunes, Pandora just to name a few, but often you hear of the artists struggling with those companies about the ownership of their music and those companies taking a nice cut of the artist's profit for their work.  MainStage is developed to have the artist first where they can choose how their content is released and when.  With the current state of quarantine, live shows are mostly cancelled or postponed;  MainStage would allow artists to continue working on music remotely and releasing content as they please.</h1>;
 }
